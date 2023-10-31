ac456affa8f665795a145bda769fa4e87a44c9f4
Jean <lawjp1@gmail.com>
2023-10-31 08:59:37 -0300

    creating project

---
+++ src/main/java/com/jecesario/generationchallenge/GenerationChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jecesario.generationchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerationChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jecesario/generationchallenge/GenerationChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jecesario.generationchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
